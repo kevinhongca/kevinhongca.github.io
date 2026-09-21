c66f29c4bf10af868da8301141d283358abdeaa2
kevinhongca <137980168+kevinhongca@users.noreply.github.com>
2026-09-21 21:32:03 +0000

    Deploying to gh-pages from @ kevinhongca/kevinhongca.github.io@5a257781fd41d226b00c835da67875ab9a062b0d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "books-the-godfather",
+  },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
